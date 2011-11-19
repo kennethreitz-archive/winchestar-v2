c5174e6b335030aa8764b0958b6083219670a3ee
Kenneth Reitz <me@kennethreitz.com>
2011-11-19 16:34:30 -0500

    ugh

--- feed.py
+++ feed.py
@@ -6,9 +6,13 @@ from datetime import datetime
 
 import dateutil.parser
 import requests
+from lxml.html import html5parser, tostring
+from lxml import etree
 
 
-LOGIN_URL = 'http://winchesterstar.com/members/login'
+HOME_URL = 'http://www.winchesterstar.com/'
+LOGIN_URL = HOME_URL + 'members/login'
+EDITION_URL = HOME_URL + 'pages/choose_edition/date:{0}'
 
 USERNAME = environ.get('WINCHESTAR_USER')
 PASSWORD = environ.get('WINCHESTAR_PASS')
@@ -54,6 +58,7 @@ class Newspaper(object):
 
         self.s.post(LOGIN_URL, data=package)
 
+
     def fetch_articles(self, date=None):
         """Returns a list of articles for the given date.
 
@@ -61,17 +66,63 @@ class Newspaper(object):
         """
 
         if date is None:
-            date = datetime.utcnow()
+            date = datetime.today()
 
         if isinstance(date, basestring):
             date = dateutil.parser.parse(unicode(date))
 
         assert isinstance(date, datetime)
 
-        print date
+        timestamp = date.strftime('%Y-%m-%d')
+        url = EDITION_URL.format(timestamp)
+        r = self.s.get(url)
+
+        print r
+        print r.content
+        # from lxml.html import tostring, html5parser
+        # t = html5parser.fromstring(r.content)
+
+
+
+        # from lxml.cssselect import CSSSelector
+
+        # print dir(t)
+
+        # print t.text
+        # print [e.get('href') for e in CSSSelector('a')(t)]
+
+        # print t.xpath('//a/@href')
+        # print t.xpath('//a')
+
+        # print t
+        # print dir(t)
+        # print t.cssselect('a')
+
+        # Convert to epic html.
+        # content = tostring(html5parser.fromstring(r.content))
+        # t = etree.fromstring(content)
+        # t.resolve_base_href()
+        # t.make_links_absolute(r.url)
+        # for tup in t.iterlinks():
+        #     print tup
+        # print dir(t)
+        # for u in t.xpath('//a'):
+            # print u
+
+        # print t.findall('a')
+        # print type(t)
+
+        # print dir(p)
+        # for url in t.iterfind('a'):
+            # print url
+        # p.getroot().make_links_absolute()
+        # print r
+        # print r.history
+        # print r.headers
+        # print r.content
 
 
 star = Newspaper()
 star.login(USERNAME, PASSWORD)
-
-star.fetch_articles("Thu Sep 25 10:36:28 BRST 2003")
+star.fetch_articles()
+# star.fetch_articles('september 3rd')
