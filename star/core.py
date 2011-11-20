a84183f4ffd253ae45f8ced297ace5e510a852a0
Kenneth Reitz <me@kennethreitz.com>
2011-11-20 14:24:34 -0500

    hmm

--- star/core.py
+++ star/core.py
@@ -8,11 +8,32 @@ The core of the star.
 """
 
 from flask import Flask
+from flaskext.sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
+
+
+class SavedArticle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140), unique=False)
+    author = db.Column(db.String(80), unique=False)
+    published = db.Column(db.DateTime(), unique=False)
+    body = db.Column(db.Text(), unique=False)
+    link = db.Column(db.String(140), unique=False)
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return '<SavedArticle %r>' % self.id
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return str(SavedArticle.query.all())
 
 if __name__ == '__main__':
     app.run()
