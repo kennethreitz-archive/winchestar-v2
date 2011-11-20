87c763c8bfa2f610d39642a6c68f5ab8c5bf4584
Kenneth Reitz <me@kennethreitz.com>
2011-11-20 14:24:42 -0500

    syncdb

--- manage.py
+++ manage.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from star import app
+from star.core import db
 from flaskext.script import Manager, Command
 
 
 manager = Manager(app)
 
+@manager.command
+def syncdb():
+    db.create_all()
 
 @manager.command
 def hello():
